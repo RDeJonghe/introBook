9e7bc6ef668f214d48bd09f1c7f540524fc53ecc
RDeJonghe <rdejonghe@hotmail.com>
2020-12-17 07:25:47 -0700

    Began Intro to JS Programming book

---
+++ README.md
@@ -0,0 +1,2 @@
+Repository for the exercises from the Introduction to Programming with JavaScript book.
+Each folder contains exercises for different chapters.
---
+++ notes/introJS_book_notes.js
@@ -0,0 +1,25 @@
+/*
+Notes for the Introduction to Programming with JavaScript book.
+Notes are organized by chapter, can search by chapter name
+  Introduction
+  Preparations
+  The Basics
+  Variables
+  Input/Output
+  Functions
+  Flow Control
+  Loops & Iterating
+  Arrays
+  Objects
+  More Stuff
+
+INTRODUCTION
+  No notes
+
+PREPARATIONS
+  No notes
+*/
+
+/*
+THE BASICS
+Every type that is not a primitive type is an object type.
