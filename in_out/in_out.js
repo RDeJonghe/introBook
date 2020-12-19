21a24aa7600404246d9087de5984f57a84f60e73
RDeJonghe <rdejonghe@hotmail.com>
2020-12-19 07:31:15 -0700

    12/19 Exercises

---
+++ in_out/in_out.js
@@ -0,0 +1,30 @@
+// INPUT / OUTPUT Exercises
+
+// 1. Write a dynamic greeter program named greeter.js. The program should ask for your name, then output Hello, {name}! where {name} is the name you entered:
+  /*
+  let name = prompt('What is your name?');
+  console.log(`Hello, ${name}!`);
+  */
+
+// 2. Modify the greeter.js program to ask for the user's first and last names separately, then greet the user with their full name.
+  /*
+  let first = prompt('What is your first name?');
+  let last = prompt('What is your last name?')
+  console.log(`Hello, ${first} ${last}!`);
+  */
+
+// 3. Modify the age.js program you wrote in the exercises for the Variables chapter.
+  /*
+  let age = Number(prompt('How old are you?'));
+  const IN_10 = age + 10;
+  const IN_20 = age + 20;
+  const IN_30 = age + 30;
+  const IN_40 = age + 40;
+  console.log(`You are ${age} years old.`);
+  console.log(`In 10 years you will be ${IN_10} years old.`);
+  console.log(`In 20 years you will be ${IN_20} years old.`);
+  console.log(`In 30 years you will be ${IN_30} years old.`);
+  console.log(`In 40 years you will be ${IN_40} years old.`);
+  */
+
+// 4. 
